ab27f021dbf9dfce08b2457570629a19180eea89
Dmitro <dimavoluychik@gmail.com>
2021-03-09 15:05:40 +0200

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template src="./components/style.html"></template>
+
+<script src="./components/scripts.js"></script>
+
+<style lang="scss" src="./components/styleFiles/style.scss"></style>
+<style src="../node_modules/bootstrap/dist/css/bootstrap.min.css"></style>
+<style src="../node_modules/@fortawesome/fontawesome-free/css/all.min.css"></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300&family=Raleway:wght@400;500;600;800&display=swap');
+</style>
---
+++ src/components/scripts.js
@@ -0,0 +1,42 @@
+export default{
+    data: function(){
+        return{
+             arrayImagesClass: ['sliderImageOne','sliderImageTwo','sliderImageThree','sliderImageFour','sliderImageFive'],
+             currentlyImage: 0,
+             activeBlock: 0,
+             loadMore: false
+            }
+    },
+
+    methods:{
+
+        nextImage(){
+
+            if(this.currentlyImage < 4){
+                this.currentlyImage++;   
+            }else{
+                this.currentlyImage = 0
+            }  
+        },
+
+        prevImage(){
+            if(this.currentlyImage > 0){
+                this.currentlyImage--;
+            }else{
+                this.currentlyImage = 4
+            }
+        },
+
+        activeBlockFn(x){
+            this.activeBlock = x;
+        }
+
+    },
+
+    computed:{
+
+        sliderHeader(){
+            return this.arrayImagesClass[this.currentlyImage];
+        }
+    }
+}
